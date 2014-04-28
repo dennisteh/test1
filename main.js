1fd7b194ff598f5524f29661dbad05c040b41560
dennisteh <quincy.89@hotmail.com>
2014-04-28 17:59:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 Test1.main = function main() {
 
-  
-//Test1.getPath('mainPage.mainPane').append() ;
+
 
 SC.routes.add(':pageName/:paneName',Test1.routes,'gotoRoute');
 SC.routes.add(':',Test1.routes,'gotoRoute');
 
 var hotelchoices =  Test1.store.find(SC.Query.local('Test1.hotel_choice')); 
-Test1.myListController.set('content', 'hotelchoices');
+Test1.myListController.set('content', hotelchoices);
 
 };
-
 
 function main() { Test1.main(); }
