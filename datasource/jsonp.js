8dacaa396be47a23571daf3156768380d522dc61
dennisteh <quincy.89@hotmail.com>
2014-04-28 11:14:17 +0800

    Create jsonp.js

---
+++ datasource/jsonp.js
@@ -0,0 +1,32 @@
+Test1.store.loadRecords('Test1.hotel_choice', [
+
+            {
+                "hotel": 4, 
+                "id": 3, 
+                "image": "http://sghotel.atlife.my/media/myhotel/images/park_royal_splash.png", 
+                "image_name": "park_royal_splash.png", 
+                "latest_version": 2, 
+                "resource_uri": "http://sghotel.atlife.my/api/myhotel/hotelsplash/3/", 
+                "sequence": 0
+            }, 
+            {
+                "hotel": 3, 
+                "id": 2, 
+                "image": "http://sghotel.atlife.my/media/myhotel/images/park_regis_splash.png", 
+                "image_name": "park_regis_splash.png", 
+                "latest_version": 2, 
+                "resource_uri": "http://sghotel.atlife.my/api/myhotel/hotelsplash/2/", 
+                "sequence": 0
+            }, 
+            {
+                "hotel": 2, 
+                "id": 1, 
+                "image": "http://sghotel.atlife.my/media/myhotel/images/days_hotel_splash.png", 
+                "image_name": "days_hotel_splash.png", 
+                "latest_version": 2, 
+                "resource_uri": "http://sghotel.atlife.my/api/myhotel/hotelsplash/1/", 
+                "sequence": 0
+            }
+        ]
+
+); 
